97a1ee8dbe9eb8f4c06a0351b9113583ab94f4b6
Danny <65949272+DannyMannn@users.noreply.github.com>
2022-03-18 19:58:38 -0600

    Adds credits and pokemon stats

--- Frontend/04 JavaScript/pokedexFetch.js
+++ Frontend/04 JavaScript/pokedexFetch.js
@@ -1,28 +1,71 @@
 const fetchPokemon = () => {
     const pokeNameInput = document.getElementById("pokemon-choice");
     let pokeName = pokeNameInput.value;
-    console.log("Poke: " + pokeName)
+    console.log("Pokemon: " + pokeName);
     pokeName = pokeName.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
-            pokeImage("./pokemon-sad.gif")
+            setPokeImage("images/pokemon_not_found.jpg");
         }
         else {
             return res.json();
         }
     }).then((data) => {
+        let stats = [];
         if (data) {
-            console.log(data);
+            data.stats.forEach(element => {
+                setStat(element.stat.name, element.base_stat);
+                stats.push(element.base_stat);
+                //setStatBar(element.stat.name, element.base_stat);
+            });
+            bars = document.querySelectorAll(".bar");
+            for (let i = 0; i < bars.length; i++){
+                bars[i].style.width = `${stats[i] * 3}px`;
+            }
+            // bars.forEach(bar => {
+            //     bar.style.width = `${stats[]}px`;
+            // });
+            // console.log(bars)
+
+            setStat("main-ability", data.abilities[0].ability.name);
+            setStat("base-experience", data.base_experience);
+            setStat("height", data.height);
+
+
             let pokeImg = data.sprites.front_default;
-            pokeImage(pokeImg);
-            console.log(pokeImg);
+            setPokeImage(pokeImg);
         }
     });
 }
 
-const pokeImage = (url) => {
+const setPokeImage = (url) => {
     const pokePhoto = document.getElementById("poke-img");
     pokePhoto.src = url;
 }
+
+const setStat = (elementId, statValue) => {
+    pokeStat = document.getElementById(elementId);
+    pokeStat.innerText = `${elementId}: ${statValue}`.toUpperCase();
+}
+
+const setStatBar = (elementId, statValue) => {
+    console.log(elementId + "-bar");
+    pokeStat = document.getElementById(elementId + "-bar")
+    console.log(pokeStat)
+
+    pokeStat.style.width = "100px";//`${statValue}px`;
+    
+}
+
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+  }
+
+
+ready(() => { 
+    let pokeFetchButton = document.getElementById("fetch-pokemon");
+    pokeFetchButton.addEventListener("click", fetchPokemon);
+  });
